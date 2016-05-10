a43304a4b746facb4c560c7c4917cda13ccb0754
Yun Zhi Lin <yun@yunspace.com>
2016-05-10 21:40:54 +1000

    use .pem for certificate instead of crt.

--- setup.sh
+++ setup.sh
@@ -55,8 +55,8 @@ fi
 if [ -n "$SSL_CERT" ]; then
   mkdir -p /usr/local/kong/ssl/
   
-  echo -e $SSL_CERT >> /usr/local/kong/ssl/server.crt
-  echo -e $SSL_KEY >> /usr/local/kong/ssl/server.key
+  echo -e $SSL_CERT > /usr/local/kong/ssl/server.pem
+  echo -e $SSL_KEY > /usr/local/kong/ssl/server.key
 
   sed -ie "s/#  ssl_cert_path:/ssl_cert_path/" /etc/kong/kong.yml
   sed -ie "s/#  ssl_key_path:/ssl_key_path/" /etc/kong/kong.yml
